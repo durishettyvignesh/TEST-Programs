3435022a8b48f821bf84913ed2e9dd248eb51551
durishettyvignesh <vigneshd0707@gmail.com>
2025-09-03 10:29:03 +0530

    Fix array reshape dimensions and enhance printing logic in 1stprogarr.py

--- arrays/1stprogarr.py
+++ arrays/1stprogarr.py
@@ -3,6 +3,9 @@
 import numpy as np
 
 arr = np.array([[1, 2, 3], [4, 5, 6]])
-arr = arr.reshape((3, 2))
+arr = arr.reshape((2, 3))
 print(arr)
 
+for i in range(arr.shape[0]):
+    for j in range(arr.shape[1]):
+        print(arr[i, j])
