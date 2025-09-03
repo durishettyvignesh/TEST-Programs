bf66de719f9c21de8d2090259c8720ddfa5863c9
durishettyvignesh <vigneshd0707@gmail.com>
2025-09-03 11:39:48 +0530

    Add filter and sorting examples using NumPy for array manipulation

---
+++ arrays/filter arrays.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+filtered_arr = arr[arr > 3]
+print(filtered_arr)
---
+++ arrays/sorting array.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr = np.array([1, 2, 10, 7, 4, 5, 6, 8, 9])
+sorted_arr = np.sort(arr)
+print(sorted_arr)
+
