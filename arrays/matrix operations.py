f7b263fb2db3a3c82f9c66f20411a224053a71b4
durishettyvignesh <vigneshd0707@gmail.com>
2025-09-03 11:45:29 +0530

    Add matrix operations examples using NumPy for array arithmetic

---
+++ arrays/matrix operations.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+c = np.add(a, b)
+print(c)
+d = a * b
+print(d)
+e = a - b
+print(e)
+f = a / b
+print(f)
+g = a @ b
+print(g)
+
