615935106dc84e6d443e8db7f719986ae130f580
durishettyvignesh <vigneshd0707@gmail.com>
2025-09-03 10:10:21 +0530

    smth

---
+++ arrays/change dt.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr = np.array([1 , 0 , 0])
+newarr = arr.astype(bool)
+print(newarr)
+print(newarr.dtype)
+
---
+++ arrays/copyarr.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.array([1,2,3,4,5])
+x = arr.copy()
+arr[0] = 42
+print(arr)
+print(x)
+# Output:
+# [42  2  3  4  5]
+# [1  2  3  4  5]
