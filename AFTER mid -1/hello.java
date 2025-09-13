6cd78b966240783fa078601b9f52a051f1697c20
durishettyvignesh <vigneshd0707@gmail.com>
2025-09-13 13:08:09 +0530

    hello added 123

---
+++ AFTER mid -1/hello.java
@@ -0,0 +1,11 @@
+package AFTER mid -1;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    // This is a simple Java program that prints
+    // "Hello, World!" to the console.
+    hello  
+}
