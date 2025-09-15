bf9a61a0c0ffdd9a32f1d5ca14c99d2be602907f
durishettyvignesh <vigneshd0707@gmail.com>
2025-09-15 11:51:47 +0530

    saved the unsaved

---
+++ AFTER mid -1/to demonstrate multithreading.java
@@ -0,0 +1,11 @@
+public class to demonstrate multithreading {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    }
+    #divide in multithreading
+
+    // This is a simple Java program that prints
+    // "Hello, World!" to the console.
+    
+}
